966aed146de6eeef09c030446ad4a501e30afa3a
Andy Evans <andrewevans0@outlook.com>
2017-03-18 10:46:17 +0000

    pointer style added

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    
-   printf("Hello, World! \n");
-   for (int i = 0; i < 10; i++) {
-       printf("Hello, World! \n");
-       printf("Hello, World! \n");
-   }
-   return 0;
+    int  var = 20;
+    int  *ip;
+
+    ip = &var;
+
+    printf("Address of var variable: %x\n", &var  );
+
+    printf("Address stored in ip variable: %x\n", ip );
+
+    printf("Value of *ip variable: %d\n", *ip );
+    printf("Hello, World! \n");
+    for (int i = 0; i < 10; i++) {
+        printf("Hello, World! \n");
+        printf("Hello, World! \n");
+    }
+    return 0;
 }
